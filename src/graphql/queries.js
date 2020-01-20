f0e21ca3bba0fe4552fec2c5f6b561edc044d57d
Neil Bateman <Neil@Rivhas-MacBook-Pro.local>
2020-01-19 19:37:06 -0800

    add Query to schema, add auth, storage

---
+++ src/graphql/queries.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getMyType = `query GetMyType($id: ID!) {
+  getMyType(id: $id) {
+    id
+    title
+    content
+    price
+    rating
+  }
+}
+`;
+export const listMyTypes = `query ListMyTypes(
+  $filter: ModelMyTypeFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listMyTypes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      title
+      content
+      price
+      rating
+    }
+    nextToken
+  }
+}
+`;
+export const speakTranslatedImageText = `query SpeakTranslatedImageText($input: SpeakTranslatedImageTextInput!) {
+  speakTranslatedImageText(input: $input)
+}
+`;
